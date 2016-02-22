ef2ea7801fe2cea4f39c2b6c96c246c2d189e488
stevej83 <jinyue306@gmail.com>
2016-02-22 11:20:47 +0800

    Create popup_insp.js

---
+++ javascripts/popup_insp.js
@@ -0,0 +1,216 @@
+function toggle208(value) {
+    if (value == 'showBad') {
+        document.getElementById('popTxt208b').style.display = 'block';
+		document.getElementById('popTxt208b2').style.display = 'none';
+        document.getElementById('badTxt208').style.display = 'block';
+        document.getElementById('popTxt208g').style.display = 'none';
+        document.getElementById('goodTxt208').style.display = 'none';
+    }
+	else if (value == 'showBad2') {
+        document.getElementById('popTxt208b').style.display = 'none';
+		document.getElementById('popTxt208b2').style.display = 'block';
+        document.getElementById('badTxt208').style.display = 'block';
+        document.getElementById('popTxt208g').style.display = 'none';
+        document.getElementById('goodTxt208').style.display = 'none';
+    }
+    else if (value == 'showGood') {
+        document.getElementById('popTxt208b').style.display = 'none';
+		document.getElementById('popTxt208b2').style.display = 'none';
+        document.getElementById('badTxt208').style.display = 'none';
+        document.getElementById('popTxt208g').style.display = 'block';
+        document.getElementById('goodTxt208').style.display = 'block';
+    }
+    else {
+        document.getElementById('popTxt208b').style.display = 'none';
+		document.getElementById('popTxt208b2').style.display = 'none';
+        document.getElementById('badTxt208').style.display = 'none';
+        document.getElementById('popTxt208g').style.display = 'none';
+        document.getElementById('goodTxt208').style.display = 'none';
+    }
+}
+
+function toggle218(value) {
+    if (value == 'showBad') {
+        document.getElementById('popTxt218b').style.display = 'block';
+		document.getElementById('popTxt218b2').style.display = 'none';
+        document.getElementById('badTxt218').style.display = 'block';
+        document.getElementById('popTxt218g').style.display = 'none';
+        document.getElementById('goodTxt218').style.display = 'none';
+    }
+	else if (value == 'showBad2') {
+        document.getElementById('popTxt218b').style.display = 'none';
+		document.getElementById('popTxt218b2').style.display = 'block';
+        document.getElementById('badTxt218').style.display = 'block';
+        document.getElementById('popTxt218g').style.display = 'none';
+        document.getElementById('goodTxt218').style.display = 'none';
+    }
+    else if (value == 'showGood') {
+        document.getElementById('popTxt218b').style.display = 'none';
+		document.getElementById('popTxt218b2').style.display = 'none';
+        document.getElementById('badTxt218').style.display = 'none';
+        document.getElementById('popTxt218g').style.display = 'block';
+        document.getElementById('goodTxt218').style.display = 'block';
+    }
+    else {
+        document.getElementById('popTxt218b').style.display = 'none';
+		document.getElementById('popTxt218b2').style.display = 'none';
+        document.getElementById('badTxt218').style.display = 'none';
+        document.getElementById('popTxt218g').style.display = 'none';
+        document.getElementById('goodTxt218').style.display = 'none';
+    }
+}
+
+function toggle228(value) {
+    if (value == 'showBad') {
+        document.getElementById('popTxt228b').style.display = 'block';
+		document.getElementById('popTxt228b2').style.display = 'none';
+        document.getElementById('badTxt228').style.display = 'block';
+        document.getElementById('popTxt228g').style.display = 'none';
+        document.getElementById('goodTxt228').style.display = 'none';
+    }
+	else if (value == 'showBad2') {
+        document.getElementById('popTxt228b').style.display = 'none';
+		document.getElementById('popTxt228b2').style.display = 'block';
+        document.getElementById('badTxt228').style.display = 'block';
+        document.getElementById('popTxt228g').style.display = 'none';
+        document.getElementById('goodTxt228').style.display = 'none';
+    }
+    else if (value == 'showGood') {
+        document.getElementById('popTxt228b').style.display = 'none';
+		document.getElementById('popTxt228b2').style.display = 'none';
+        document.getElementById('badTxt228').style.display = 'none';
+        document.getElementById('popTxt228g').style.display = 'block';
+        document.getElementById('goodTxt228').style.display = 'block';
+    }
+    else {
+        document.getElementById('popTxt228b').style.display = 'none';
+		document.getElementById('popTxt228b2').style.display = 'none';
+        document.getElementById('badTxt228').style.display = 'none';
+        document.getElementById('popTxt228g').style.display = 'none';
+        document.getElementById('goodTxt228').style.display = 'none';
+    }
+}
+
+function toggle238(value) {
+    if (value == 'showBad') {
+        document.getElementById('popTxt238b').style.display = 'block';
+		document.getElementById('popTxt238b2').style.display = 'none';
+        document.getElementById('badTxt238').style.display = 'block';
+        document.getElementById('popTxt238g').style.display = 'none';
+        document.getElementById('goodTxt238').style.display = 'none';
+    }
+	else if (value == 'showBad2') {
+        document.getElementById('popTxt238b').style.display = 'none';
+		document.getElementById('popTxt238b2').style.display = 'block';
+        document.getElementById('badTxt238').style.display = 'block';
+        document.getElementById('popTxt238g').style.display = 'none';
+        document.getElementById('goodTxt238').style.display = 'none';
+    }
+    else if (value == 'showGood') {
+        document.getElementById('popTxt238b').style.display = 'none';
+		document.getElementById('popTxt238b2').style.display = 'none';
+        document.getElementById('badTxt238').style.display = 'none';
+        document.getElementById('popTxt238g').style.display = 'block';
+        document.getElementById('goodTxt238').style.display = 'block';
+    }
+    else {
+        document.getElementById('popTxt238b').style.display = 'none';
+		document.getElementById('popTxt238b2').style.display = 'none';
+        document.getElementById('badTxt238').style.display = 'none';
+        document.getElementById('popTxt238g').style.display = 'none';
+        document.getElementById('goodTxt238').style.display = 'none';
+    }
+}
+
+function toggle248(value) {
+    if (value == 'showBad') {
+        document.getElementById('popTxt248b').style.display = 'block';
+		document.getElementById('popTxt248b2').style.display = 'none';
+        document.getElementById('badTxt248').style.display = 'block';
+        document.getElementById('popTxt248g').style.display = 'none';
+        document.getElementById('goodTxt248').style.display = 'none';
+    }
+	else if (value == 'showBad2') {
+        document.getElementById('popTxt248b').style.display = 'none';
+		document.getElementById('popTxt248b2').style.display = 'block';
+        document.getElementById('badTxt248').style.display = 'block';
+        document.getElementById('popTxt248g').style.display = 'none';
+        document.getElementById('goodTxt248').style.display = 'none';
+    }
+    else if (value == 'showGood') {
+        document.getElementById('popTxt248b').style.display = 'none';
+		document.getElementById('popTxt248b2').style.display = 'none';
+        document.getElementById('badTxt248').style.display = 'none';
+        document.getElementById('popTxt248g').style.display = 'block';
+        document.getElementById('goodTxt248').style.display = 'block';
+    }
+    else {
+        document.getElementById('popTxt248b').style.display = 'none';
+		document.getElementById('popTxt248b2').style.display = 'none';
+        document.getElementById('badTxt248').style.display = 'none';
+        document.getElementById('popTxt248g').style.display = 'none';
+        document.getElementById('goodTxt248').style.display = 'none';
+    }
+}
+
+function toggle258(value) {
+    if (value == 'showBad') {
+        document.getElementById('popTxt258b').style.display = 'block';
+		document.getElementById('popTxt258b2').style.display = 'none';
+        document.getElementById('badTxt258').style.display = 'block';
+        document.getElementById('popTxt258g').style.display = 'none';
+        document.getElementById('goodTxt258').style.display = 'none';
+    }
+	else if (value == 'showBad2') {
+        document.getElementById('popTxt258b').style.display = 'none';
+		document.getElementById('popTxt258b2').style.display = 'block';
+        document.getElementById('badTxt258').style.display = 'block';
+        document.getElementById('popTxt258g').style.display = 'none';
+        document.getElementById('goodTxt258').style.display = 'none';
+    }
+    else if (value == 'showGood') {
+        document.getElementById('popTxt258b').style.display = 'none';
+		document.getElementById('popTxt258b2').style.display = 'none';
+        document.getElementById('badTxt258').style.display = 'none';
+        document.getElementById('popTxt258g').style.display = 'block';
+        document.getElementById('goodTxt258').style.display = 'block';
+    }
+    else {
+        document.getElementById('popTxt258b').style.display = 'none';
+		document.getElementById('popTxt258b2').style.display = 'none';
+        document.getElementById('badTxt258').style.display = 'none';
+        document.getElementById('popTxt258g').style.display = 'none';
+        document.getElementById('goodTxt258').style.display = 'none';
+    }
+}
+
+function toggle268(value) {
+    if (value == 'showBad') {
+        document.getElementById('popTxt268b').style.display = 'block';
+		document.getElementById('popTxt268b2').style.display = 'none';
+        document.getElementById('badTxt268').style.display = 'block';
+        document.getElementById('popTxt268g').style.display = 'none';
+        document.getElementById('goodTxt268').style.display = 'none';
+    }
+	else if (value == 'showBad2') {
+        document.getElementById('popTxt268b').style.display = 'none';
+		document.getElementById('popTxt268b2').style.display = 'block';
+        document.getElementById('badTxt268').style.display = 'block';
+        document.getElementById('popTxt268g').style.display = 'none';
+        document.getElementById('goodTxt268').style.display = 'none';
+    }
+    else if (value == 'showGood') {
+        document.getElementById('popTxt268b').style.display = 'none';
+		document.getElementById('popTxt268b2').style.display = 'none';
+        document.getElementById('badTxt268').style.display = 'none';
+        document.getElementById('popTxt268g').style.display = 'block';
+        document.getElementById('goodTxt268').style.display = 'block';
+    }
+    else {
+        document.getElementById('popTxt268b').style.display = 'none';
+		document.getElementById('popTxt268b2').style.display = 'none';
+        document.getElementById('badTxt268').style.display = 'none';
+        document.getElementById('popTxt268g').style.display = 'none';
+        document.getElementById('goodTxt268').style.display = 'none';
+    }
+}
